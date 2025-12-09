2051e5cc22870992e660465becacc551e906eebc
Sheikh Muktadir <your-email@example.com>
2025-12-09 11:23:50 +0400

    responsive fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import { FiPhoneCall } from "react-icons/fi";
 import { FaWhatsapp } from "react-icons/fa";
+import Container from "./Components/Container/Container";
 
 export default function Layout() {
   const { pathname } = useLocation();
@@ -20,17 +21,21 @@ export default function Layout() {
       </main>
       <Footer />
       <div className="mobileBottomContact">
-        <a href="tel:+971587772622" className="mobileBottomText">
-          <span>Call Us Now</span>
-          <FiPhoneCall />
-        </a>
-        <a
-          href="https://wa.me/911234567890?text=Hello%2C+I+want+to+know+more+about+the+property"
-          className="mobileBottomText"
-        >
-          <span>Whatsapp</span>
-          <FaWhatsapp />
-        </a>
+        <Container>
+          <div className="mobileBottomContactFlex">
+            <a href="tel:+971587772622" className="mobileBottomText">
+              <span>Call Us Now</span>
+              <FiPhoneCall />
+            </a>
+            <a
+              href="https://wa.me/911234567890?text=Hello%2C+I+want+to+know+more+about+the+property"
+              className="mobileBottomText"
+            >
+              <span>Whatsapp</span>
+              <FaWhatsapp />
+            </a>
+          </div>
+        </Container>
       </div>
     </>
   );
